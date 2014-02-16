7651773684ccff5c53448417105c23b4717b3f8c
Lasm Gratel <lasm_gratel@163.com>
2014-02-16 13:39:41 +0800

    The first version of MyBookManager. Is create by Netbeans and Eclipse.

---
+++ MyBookManager/src/com/lasmgratel/depot/main/Book.java
@@ -0,0 +1,58 @@
+package com.lasmgratel.depot.main;
+public class Book{
+	private String name;
+	private int amount;
+	private int price;
+	public Book(){
+	}
+	public Book(String name,int amount,int price){
+		setName(name);setAmount(amount);setPrice(price);
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + amount;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + price;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (amount != other.amount)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (price != other.price)
+			return false;
+		return true;
+	}	
+}
